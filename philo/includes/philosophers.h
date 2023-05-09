a5ad18db0b7cd0a22c9edc9e300c6f3ce63aa676
ffouquet42 <ffouquet42@gmail.com>
2023-05-09 09:03:29 +0200

    create .h and main

---
+++ philo/includes/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 08:57:57 by fllanet           #+#    #+#             */
+/*   Updated: 2023/05/09 09:00:07 by fllanet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <limits.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+#endif
