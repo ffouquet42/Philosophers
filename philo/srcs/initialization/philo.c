2902d7d6c16e7932c4000d9804e1faf58aa6c221
ffouquet42 <ffouquet42@gmail.com>
2023-05-10 13:50:10 +0200

    init philo done

--- philo/srcs/initialization/philo.c
+++ philo/srcs/initialization/philo.c
@@ -1,27 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   setup.c                                            :+:      :+:    :+:   */
+/*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/09 11:58:28 by fllanet           #+#    #+#             */
-/*   Updated: 2023/05/10 13:49:27 by fllanet          ###   ########.fr       */
+/*   Created: 2023/05/10 13:35:47 by fllanet           #+#    #+#             */
+/*   Updated: 2023/05/10 13:47:11 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philosophers.h"
 
-int	setup(char **argv)
+t_philo	*init_philo(t_data *data)
 {
-	t_data	*data;
 	t_philo	*philo;
+	int		i;
 	
-	data = init_data(argv);
-	if (!data)
-		return (1);
-	philo = init_philo(data);
+	i = 0;
+	philo = ft_calloc(sizeof(t_philo), data->nb_of_philo);
 	if (!philo)
-		return (free_data(data), 1);
-	return (free_data(data), free(philo), 0);
+		return (printf("Philo init : malloc failed\n"), NULL);
+	while (i < data->nb_of_philo)
+	{
+		philo[i].id = i + 1;
+		philo[i].nb_of_meals = 0;
+		philo[i].last_eat_time = 0;
+		philo[i].data_struct = data;
+		i++;
+	}
+	return (philo);
 }
